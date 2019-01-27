a44de6913d7c9a318448403bcc010292a5595d64
GedMarc <ged_marc@hotmail.com>
2019-01-27 10:13:37 +0200

    module-info removal of import statements

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -1,11 +1,3 @@
-import com.jwebmp.core.base.angular.services.IAngularModule;
-import com.jwebmp.core.services.IPageConfigurator;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
-import com.jwebmp.plugins.angularprettycheckboxes.AngularPrettyCheckboxesModule;
-import com.jwebmp.plugins.angularprettycheckboxes.AngularPrettyCheckboxesPageConfigurator;
-import com.jwebmp.plugins.angularprettycheckboxes.implementations.AngularPrettyCheckboxesExclusionsModule;
-
 module com.jwebmp.plugins.angularprettycheckboxes {
 	exports com.jwebmp.plugins.angularprettycheckboxes;
 
@@ -14,12 +6,12 @@ module com.jwebmp.plugins.angularprettycheckboxes {
 	requires com.jwebmp.guicedinjection;
 	requires com.jwebmp.core.angularjs;
 
-	provides IPageConfigurator with AngularPrettyCheckboxesPageConfigurator;
-	provides IAngularModule with AngularPrettyCheckboxesModule;
+	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.angularprettycheckboxes.AngularPrettyCheckboxesPageConfigurator;
+	provides com.jwebmp.core.base.angular.services.IAngularModule with com.jwebmp.plugins.angularprettycheckboxes.AngularPrettyCheckboxesModule;
 
 	opens com.jwebmp.plugins.angularprettycheckboxes to com.fasterxml.jackson.databind, com.jwebmp.core;
 
-	provides IGuiceScanModuleExclusions with AngularPrettyCheckboxesExclusionsModule;
-	provides IGuiceScanJarExclusions with AngularPrettyCheckboxesExclusionsModule;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.angularprettycheckboxes.implementations.AngularPrettyCheckboxesExclusionsModule;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.angularprettycheckboxes.implementations.AngularPrettyCheckboxesExclusionsModule;
 
 }
