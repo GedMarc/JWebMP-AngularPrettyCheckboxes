3dee077efeac77c3f72a2d0eb12c196094e688d1
GedMarc <ged_marc@hotmail.com>
2019-02-09 11:52:03 +0200

    The Website API Brings out many things....

--- src/main/java/com/jwebmp/plugins/angularprettycheckboxes/AngularPrettyCheckboxesPageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/angularprettycheckboxes/AngularPrettyCheckboxesPageConfigurator.java
@@ -87,7 +87,7 @@ public class AngularPrettyCheckboxesPageConfigurator
 	@Override
 	public Page configure(Page page)
 	{
-		if (!page.isConfigured())
+		if (!page.isConfigured() && enabled())
 		{
 			JQueryPageConfigurator.setRequired(true);
 			AngularPageConfigurator.setRequired(true);
